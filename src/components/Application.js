0a59ec5cc36531a6a3102c0bee542680b3c12d05
sharkinsspatial <sharkinsgis@gmail.com>
2015-10-14 10:09:40 -0300

    Included Navbar.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,5 @@
 import alt from '../alt'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import {Router, Route, IndexRoute} from 'react-router'
 import ReportStore from '../stores/ReportStore'
 import ParkingLotStore from '../stores/ParkingLotStore'
@@ -12,7 +11,10 @@ import Panel from 'react-bootstrap/lib/Panel'
 import ReportMap from './ReportMap'
 import AltContainer from 'alt/AltContainer'
 import ReportActions from '../actions/ReportActions'
-
+import Navbar from 'react-bootstrap/lib/Navbar'
+import Nav from 'react-bootstrap/lib/Nav'
+import NavItem from 'react-bootstrap/lib/NavItem'
+import {LinkContainer} from 'react-router-bootstrap'
 
 class Application extends React.Component {
     componentDidMount() {
@@ -22,6 +24,16 @@ class Application extends React.Component {
     render() {
         return (
             <div>
+                <Navbar toggleNavKey={0}>
+                <Nav right eventKey={0}>
+                    <LinkContainer to='/create'>
+                        <NavItem eventKey={1}>Create Report</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to='/#'>
+                        <NavItem eventKey={2}>Reports</NavItem>
+                    </LinkContainer>
+                </Nav>
+                </Navbar>
                 <Row>
                 <Col md={4}>
                 <Panel>
@@ -40,7 +52,7 @@ class Application extends React.Component {
     }
 }
 
-ReactDOM.render((
+React.render((
     <Router>
         <Route path='/' component={Application}>
             <IndexRoute component={ReportListContainer}/>
