832d618dfae27b1c209f6cb09ccb783cf771fa56
sharkinsspatial <sharkinsgis@gmail.com>
2015-11-05 14:27:56 -0300

    Fixed Facebook login component and added loading animation.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import classNames from 'classnames'
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { loading: false }
+    }
+
     render() {
         let loginUrl = 'http://localhost:3000/auth/facebook'
+        let btnClass = classNames({
+            'fa': true,
+            'fa-facebook': !this.state.loading,
+            'fa-refresh': this.state.loading,
+            'fa-spin': this.state.loading
+        })
         return (
-            <div className="login">
-                <a className='btn btn-lrg btn-social btn-google' href={loginUrl}>
-                <i className='fa fa-google'></i>Login With Google</a>
+            <div className='text-center'>
+                <a className='btn btn-lrg btn-social btn-facebook'
+                    href={loginUrl} onClick={this.handleClick}>
+                <i ref='btn' className={btnClass}></i>Login with Facebook</a>
             </div>
         )
+    }
+    handleClick = () => {
+        this.setState({ loading: true })
     }
 }
 
