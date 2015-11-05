04d41414fd67e8e505f51385d7a74d3d3deb2b9a
sharkinsspatial <sharkinsgis@gmail.com>
2015-11-05 17:26:08 -0300

    Fixed test for RouteControls.

--- src/components/__tests__/RouteControls-test.js
+++ src/components/__tests__/RouteControls-test.js
@@ -14,27 +14,36 @@ describe('RouteControls', () => {
     var ParkingLotActions = {}
     var ParkingLots = {}
     ParkingLots.parkinglots = [{name: 'P8', id: 1}]
+    var ReportActions = {}
+    var Reports = {}
+    Reports.reportRouteDistance = 0
+    var propsObject = {}
     beforeEach(() => {
         ParkingLotActions.setActiveParkingLot = jest.genMockFunction()
+        ReportActions.setReportRouteDistance = jest.genMockFunction()
+        propsObject = { ParkingLots: ParkingLots,
+                        ParkingLotActions: ParkingLotActions,
+                        Reports: Reports,
+                        ReportActions: ReportActions }
     })
 
     it('Calls the setActiveParkingLot function prop when a parking lot is selected',
         () => {
             let instance = TestUtils.renderIntoDocument(
                <RouteControls ParkingLots={ParkingLots}
-                ParkingLotActions={ParkingLotActions}/>
+                ParkingLotActions={ParkingLotActions} Reports={Reports}
+                ReportActions={ReportActions}/>
             )
             let li = TestUtils.findRenderedDOMComponentWithTag(instance, 'li')
             TestUtils.Simulate.select(li)
             expect(ParkingLotActions.setActiveParkingLot.mock.calls.length)
-                .toEqual(2)
+                .toEqual(1)
     })
 
     it('Should render one list item when passed a single parking lot prop',
         () => {
             const tree = sd.shallowRender(React.createElement(RouteControls,
-                                                {ParkingLots: ParkingLots,
-                                                ParkingLotActions: ParkingLotActions}))
+                                            propsObject))
             const vdom = tree.getRenderOutput()
             const input = vdom.props.children
             const dropDown = input.props.children.filter(dropDown => TestUtils
@@ -43,16 +52,16 @@ describe('RouteControls', () => {
                 .isElementOfType(menuItem, MenuItem))
             expect(menuItems.length).toEqual(1)
     })
-
-    it('Should clear the active parking lot when the component is mounted',
-       () => {
-            const shallowRenderer = TestUtils.createRenderer()
-            shallowRenderer.render(React.createElement(RouteControls,
-                                                {ParkingLots: ParkingLots,
-                                        ParkingLotActions: ParkingLotActions}))
-            expect(ParkingLotActions.setActiveParkingLot.mock.calls.length)
-                .toEqual(1)
-            expect(ParkingLotActions.setActiveParkingLot.mock.calls[0])
-                   .toEqual({})
-    })
+    //Need to upgrade React to version where shallowRenderer supports
+    //getMountedInstance
+    //it('Should clear the active parking lot when the component is unmounted',
+       //() => {
+            //const shallowRenderer = TestUtils.createRenderer()
+            //shallowRenderer.render(React.createElement(RouteControls,
+                        //propsObject))
+            //expect(ParkingLotActions.setActiveParkingLot.mock.calls.length)
+                //.toEqual(1)
+            //expect(ParkingLotActions.setActiveParkingLot.mock.calls[0])
+                   //.toEqual({})
+    //})
 })
