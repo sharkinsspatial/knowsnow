7811ff68d87f85a3d8fbd2533d852d87acbf252e
sharkinsspatial <sharkinsgis@gmail.com>
2015-11-11 11:40:07 -0300

    Included s3 upload task for production deployment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,13 +62,76 @@ module.exports = function(grunt) {
                         dest: 'dist/'}
                 ]
             }
+        },
+        aws: grunt.file.readJSON('/Users/Sharkins/Documents/AWS/grunt-aws.json'),
+        aws_s3: {
+            options: {
+                accessKeyId: '<%= aws.key %>',
+                secretAccessKey: '<%= aws.secret %>',
+                bucket: 'knowsnow',
+                access: 'public-read',
+                params: {
+                    // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+                    CacheControl: "max-age=630720000, public",
+                    Expires: new Date(Date.now() + 63072000000).toISOString()
+                }
+            },
+            dist: {
+                files: [
+                {
+                    src: 'index.html',
+                    dest: 'index.html'
+                },
+                {
+                    src: 'dist/*',
+                    dest: '/'
+                },
+                {
+                    src: 'css/*',
+                    dest: '/'
+                },
+                {
+                    src: 'node_modules/leaflet/dist/*.css',
+                    dest: '/'
+                },
+                {
+                    src: 'node_modules/leaflet/dist/images/*.png',
+                    dest: '/'
+                },
+                {
+                    src: 'node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.css',
+                    dest: '/'
+                },
+                {
+                    src: 'node_modules/drmonty-leaflet-awesome-markers/css/**',
+                    dest: '/'
+                },
+                {
+                    src: 'node_modules/react-bootstrap-datetimepicker/css/bootstrap-datetimepicker.css',
+                    dest: '/'
+                },
+                {
+                    src: 'node_modules/babel-polyfill/dist/polyfill.min.js',
+                    dest: '/'
+                }]
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/main.js': ['dist/main.js']
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-aws-s3');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('dev', ['env:dev', 'browserify:dev', 'replace:dev']);
-    grunt.registerTask('dist', ['env:dist', 'browserify:dist', 'replace:dist'])
+    grunt.registerTask('dist', ['env:dist', 'browserify:dist', 'replace:dist',
+        'uglify:dist', 'aws_s3:dist'])
 };
